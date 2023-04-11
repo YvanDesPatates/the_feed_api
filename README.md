239b6730e574b50dfb9b2a2558a1dfd1c3b62db0
roux <yvan.roux@inrae.fr>
2023-04-11 12:26:45 +0200

    Publications

---
+++ README.md
@@ -0,0 +1,11 @@
+# The feed API
+similar to Twitter API ... almost similar :)
+
+## install
+configure the .env.local to set up the database
+
+run :
+- php bin/console make:migration
+- php bin/console doctrine:migrations:migrate
+
+warning ! it will erase everything on the table
---
+++ src/Entity/Publication.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Repository\PublicationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PublicationRepository::class)]
+#[ApiResource(operations: [new GetCollection(), new Get(), new Delete()], order: ["datePublication" => "DESC"])]
+class Publication
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\Length(min: 4, max: 200)]
+    #[Assert\NotBlank]
+    #[Assert\NotNull]
+    #[ORM\Column(length: 200)]
+    private ?string $message = null;
+
+    #[ApiProperty(writable: false)]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datePublication = null;
+
+    public function __construct()
+    {
+        $this->setDatePublication(new \DateTime());
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTimeInterface
+    {
+        return $this->datePublication;
+    }
+
+    public function setDatePublication(\DateTimeInterface $datePublication): self
+    {
+        $this->datePublication = $datePublication;
+
+        return $this;
+    }
+}
